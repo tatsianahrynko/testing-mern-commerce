0082f4db928ded6c90f4d7a7d2b86e6a6bb58f82
tatsianahrynko <tatsiana.hrynko@gmail.com>
2023-04-11 16:59:11 -0700

    init commit TH

--- test/address.spec.js
+++ test/address.spec.js
@@ -1,25 +1,26 @@
+const { url } = require('inspector');
 const superagent = require('superagent');
 
-const baseUrl = "https://mern-ecommerce.sdet.school/api";
+const baseUrl = "https://mern-ecommerce.sdet.school/api"
 
 describe("Test address endpoints", ()=> {
-    let token;
+    let token; //token now undefined
     beforeEach(async ()=> {
-        // Login as  Olsen  X
+        
+        //login as Tatsiana
         const reqBody = {
-            email: "Olsen.X@gmail.com",
-            password: "Password1"
+            email: "tatianautest@gmail.com",
+            password: "qwEsz_123"
           }
           try {
-            const response = await superagent.post(baseUrl+"/auth/login").send(reqBody);
-            token = response.body.token;
-          } catch (error) {
+            const response = await superagent.post(baseUrl + "/auth/login").send(reqBody);
+            token = (response.body.token);
+          }catch (error) {
             console.log("catch?")
             console.error(error.message);
           }
     });
-    
-    it("should run test",()=>{
+    it("should run test", ()=>{
         console.log(token);
     })
 });
