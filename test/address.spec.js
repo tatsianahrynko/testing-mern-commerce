b1df13b754b0fbe611579814b9e3aa33c9741bbc
tatsianahrynko <tatsiana.hrynko@gmail.com>
2023-04-11 23:02:10 -0700

    Added address

--- test/address.spec.js
+++ test/address.spec.js
@@ -1,3 +1,8 @@
+const chai = require('chai'); //imported chai
+const chaiSubset = require('chai-subset');
+chai.use(chaiSubset);
+const expect =  chai.expect;
+const casual = require('casual'); 
 const { url } = require('inspector');
 const superagent = require('superagent');
 
@@ -5,9 +10,8 @@ const baseUrl = "https://mern-ecommerce.sdet.school/api"
 
 describe("Test address endpoints", ()=> {
     let token; //token now undefined
-    beforeEach(async ()=> {
+    beforeEach(async ()=> {    
         
-        //login as Tatsiana
         const reqBody = {
             email: "tatianautest@gmail.com",
             password: "qwEsz_123"
@@ -20,7 +24,48 @@ describe("Test address endpoints", ()=> {
             console.error(error.message);
           }
     });
-    it("should run test", ()=>{
-        console.log(token);
+    it("should add address to user", async ()=>{
+      // const street = casual.street;
+      // const city = casual.city;
+      //const state = casual.state
+      const {street , city, state} = casual; //distructors
+      const zip =  casual.zip(5);
+
+        const addressOpt = { //request body
+          isDefault: true,
+          address: street,
+          city: city,
+          state: state,
+          country: "US",
+          zipCode: zip
+}
+//console.log("addressOpt", addressOpt);
+let response; // to make it visible outside
+//making a call
+try {
+  response = await superagent.post(baseUrl+"/address/add")
+  .set({
+    Authorization: token
+  })
+  .send(addressOpt)
+   
+}catch(err){
+  console.log(err.message)
+}
+//console.log(response.body)
+expect(response.body).to.containSubset({
+    success: true,
+    message: 'Address has been added successfully!',
+    address: {
+      isDefault: true,
+      address: street,
+      city: city,
+      state: state,
+      country: 'US',
+      zipCode: zip,
+      user: '6435b24a3ea9a80035112d6e',
+        __v: 0
+    }    
+})
     })
 });
