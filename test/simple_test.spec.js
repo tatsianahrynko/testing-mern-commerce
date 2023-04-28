4a809b9bc27b2194c5a49448fc808937f03fe1a1
tatsianahrynko <tatsiana.hrynko@gmail.com>
2023-04-27 22:45:46 -0700

    Playwright

--- test/simple_test.spec.js
+++ test/simple_test.spec.js
@@ -4,41 +4,56 @@ const username = 'Jerome20@hotmail.com';
 const password = 'Password1';
 const sleep = async (ms) => { return new Promise((resolve) => { return setTimeout (resolve, ms); }); };
 
-describe('Very simple test', () => {
-    it('should make a simple test', async() => {
-        console.log('hello, world');
+describe('Login test', () => {
+    let browser; //visible variables
+    let context;  
+    let page;
+    beforeEach(async() => {
         const browserType = browsers['webkit'];
-        
-        const browser = await browserType.launch({
+        browser = await browserType.launch({
             headless: false
         });
         console.log(browser.version());
-        const context = await browser.newContext({
+        context = await browser.newContext({
+            //setTimeout: 5000,
             viewport: {
-                width:1000,
+                width:1200,
                 height:800
             }
         });
-        const page = await context.newPage();
+        await context.setDefaultTimeout(5000);
+    });      
+    
+    afterEach(async () => {
+        await sleep (2000);
+        await page.close();
+        await browser.close();
+    });
+
+    it('should login to user dashboard', async() => {           
+        page = await context.newPage();
         await page.goto('https://mern-ecommerce.sdet.school/');
         await page.getByText('Welcome!').click();
         await page.getByText('Login').click();
         await page.locator('(//*[@name="email"])[1]').fill(username);
         await page.locator('(//*[@name="password"])[1]').fill(password);
         await page.locator('(//*[@type="submit"])[1]').click();
-        //await page.getByText('Login').click();
-        //await page.press('Enter');
-
-        //const element = await page.getByText('Account Info');
-        //const element = await page.locator('(//*[@class="nav-link"])[3]');
+        
         const element = await page.locator('//*[@class="role member "]');
         
         console.log(await element.innerText());
-        //expect(await element.isVisible()).to.be.true;
-        expect(await element.textContent()).to.equal('Member');
+        expect(await element.textContent()).to.equal('Member');        
+    });
 
-        await sleep (5000);
-        page.close();
-        browser.close();
+    it('should show error notification', async() => {           
+        page = await context.newPage();
+        await page.goto('https://mern-ecommerce.sdet.school/');
+        await page.getByText('Welcome!').click();
+        await page.getByText('Login').click();
+        await page.locator('(//*[@name="email"])[1]').fill(username);
+        await page.locator('(//*[@name="password"])[1]').fill(password + '1');
+        await page.locator('(//*[@type="submit"])[1]').click();
+        
+        await page.locator('.notification-error').waitFor();  
     });
 });
